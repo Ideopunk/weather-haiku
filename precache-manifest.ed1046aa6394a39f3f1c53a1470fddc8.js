042c80456de0a815e0a13183d431a2a23af83dbc
Conor Barnes <conorbarnes93@gmail.com>
2020-08-22 17:42:17 -0400

    Updates

--- precache-manifest.ed1046aa6394a39f3f1c53a1470fddc8.js
+++ precache-manifest.ed1046aa6394a39f3f1c53a1470fddc8.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0534b7fc007d6c0d968c6913b25d8960",
+    "revision": "94da3eb5a3da63548c3f651800c386cc",
     "url": "/weathering-with-haiku/index.html"
   },
   {
-    "revision": "82394ad90e2061e21fe6",
-    "url": "/weathering-with-haiku/static/css/main.e5d670c0.chunk.css"
+    "revision": "ab86a09cac2d5b88bcea",
+    "url": "/weathering-with-haiku/static/css/main.dee53877.chunk.css"
   },
   {
     "revision": "bcee290dd986e65aebc3",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/weathering-with-haiku/static/js/2.afe039e6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "82394ad90e2061e21fe6",
+    "revision": "ab86a09cac2d5b88bcea",
     "url": "/weathering-with-haiku/static/js/main.6e9efb91.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/weathering-with-haiku/precache-manifest.0c0cd18588d9b7cd45716137ff3a1053.js"
+  "/weathering-with-haiku/precache-manifest.ed1046aa6394a39f3f1c53a1470fddc8.js"
 );
 
 self.addEventListener('message', (event) => {
